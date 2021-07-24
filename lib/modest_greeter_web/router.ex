32cfc2c14fb6fd0a45b8bc918d5dc4f16eb424e8
letitride <ichikawa.fumiya@gmail.com>
2021-07-24 15:36:33 +0900

    第10章 webpack

---
+++ lib/modest_greeter_web/router.ex
@@ -0,0 +1,18 @@
+defmodule ModestGreeterWeb.Router do
+  use ModestGreeterWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", ModestGreeterWeb do
+    pipe_through :browser
+    get "/hello", HelloController, :show
+    get "/hello/:name", HelloController, :show
+  end
+
+end
